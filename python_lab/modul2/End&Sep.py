1f5108caf1b1716b89d48963bb72a2ca59a5c48b
yssabela <114014540+yssabela@users.noreply.github.com>
2023-06-05 14:59:22 +0300

    PythonLab

---
+++ python_lab/modul2/End&Sep.py
@@ -0,0 +1,14 @@
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+print()
+print("Programming","Essentials","in",sep="***",end="...")
+print("Python")
+
+
+print()
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+print()
+print("My", "name", "is", "Monty", "Python.", sep="/")
+print()
+print("My", "name", "is", "Monty", "Python.", sep="")
